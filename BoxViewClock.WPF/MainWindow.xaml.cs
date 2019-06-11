4afb06b45ba95673c552c28fb18eb150af813a15
jrahnpdv <51696826+jrahnpdv@users.noreply.github.com>
2019-06-11 10:15:33 +0200

    init

---
+++ BoxViewClock.WPF/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace BoxViewClock.WPF
+{
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            LoadApplication(new BoxViewClock.App());
+        }
+    }
+}
---
+++ BoxViewClock/BoxViewClock/Page1.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BoxViewClock
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+
+        private Dictionary<object, bool> Counter = new Dictionary<object, bool>();
+
+		public Page1 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void OnClicked(object sender, EventArgs e)
+        {
+            var btn = (Button)sender;
+            var isBlack = Counter.ContainsKey(sender) ? Counter[sender] : true;
+            Counter[sender] = !isBlack;
+            btn.BackgroundColor = isBlack ? Color.Black : Color.White;
+            btn.ImageSource = isBlack ? ImageSource.FromFile("check_white.png") : ImageSource.FromFile("check.png");
+        }
+    }
+}
